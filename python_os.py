c96775cb12776137c63c2daa0781814551a9c2d5
RhondaFischer <rhonda.k.fischer@gmail.com>
2020-02-01 21:02:06 -0500

    Update through PyCharm

--- python_os.py
+++ python_os.py
@@ -7,15 +7,15 @@ class Provider:
 
     def list(self):
         banner("list")
-        # os.system("multipass find")
+        os.system("multipass find")
 
     def shell(self):
         print("shell")
-        # os.system("multipass shell")
+        #os.system("multipass shell")
 
     def run(self, command):
         print(f"run {command}")
-        # os.system(f"multipass exec {command}")
+        #os.system(f"multipass exec {command}")
 
 
 if __name__ == "__main__":
@@ -23,19 +23,26 @@ if __name__ == "__main__":
     p.run("hallo")
     p.list()
 
-curr_dir = os.getcwd()
+
 opsys_name = os.name
 oplat = sys.platform
-filesize = sys.getsizeof("cm")
-print(opsys_name, oplat, curr_dir)
-print(filesize)
+curr_dir = os.getcwd()
+print("Op name, platform, current dir:", opsys_name, oplat, curr_dir)
 
-list = os.listdir('/')
+filesize = sys.getsizeof("cm")
+filesize2 = sys.getsizeof("/")
+print("Filesize:", filesize, filesize2)
+
+x = Provider()
+x.list()
+
+#list = os.listdir('/')
 #result=os.statvfs('/')
 #result=os.fstat('/')
 #block_size=result.f_frsize
 #total_blocks=result.f_blocks
 #free_blocks=result.f_bfree
-print(list)
+#print(list)
 #print(result)
 #block_size, total_blocks, free_blocks
+
